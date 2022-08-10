4366038c8449fdf43040f4a4aebb1212696019d2
thiakovidis <thiakovidis@agroapps.gr>
2022-08-10 20:43:00 +0300

    Project setup

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcome container">Welcome</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
