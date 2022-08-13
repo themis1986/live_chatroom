ac659c1bfbe7ba53a15a16c2552e1371cc90c305
thiakovidis <thiakovidis@agroapps.gr>
2022-08-13 18:24:52 +0300

    Added auto scrolling to bottom in chat window

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div v-if="documents" class="messages">
+    <div v-if="formattedDocuments" class="messages" ref="messages">
       <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
         <span class="created-at">{{ doc.createdAt }}</span>
         <span class="name">{{ doc.name }}</span>
@@ -12,9 +12,9 @@
 </template>
 
 <script>
+import { ref, onUpdated, computed } from "vue";
 import getCollection from "../composables/getCollection";
 import { formatDistanceToNow } from "date-fns";
-import { computed } from "vue";
 
 export default {
   setup() {
@@ -29,7 +29,13 @@ export default {
       }
     });
 
-    return { error, documents, formattedDocuments };
+    // autoscroll to bottom messages
+    const messages = ref(null);
+    onUpdated(() => {
+      messages.value.scrollTop = messages.value.scrollHeight;
+    });
+
+    return { error, documents, formattedDocuments, messages };
   },
 };
 </script>
