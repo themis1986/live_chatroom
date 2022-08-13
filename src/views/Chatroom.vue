af9d3ead822eb2d0ad602c385bb906638a96cb30
thiakovidis <thiakovidis@agroapps.gr>
2022-08-13 16:49:33 +0300

    Redirected to welcome view when a user is logged out

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,10 +5,21 @@
 </template>
 
 <script>
+import { watch } from "vue";
+import { useRouter } from "vue-router";
 import Navbar from "../components/Navbar.vue";
+import getUser from "../composables/getUser";
 export default {
   components: { Navbar },
   setup() {
+    const { user } = getUser();
+    const router = useRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
     return {};
   },
 };
