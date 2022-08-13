4872a2062ee14b1a4622742964e80212403a245c
thiakovidis <thiakovidis@agroapps.gr>
2022-08-13 18:39:30 +0300

    Added functionality for unsubscribing from real time listeners every time a user logs out

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { projectFirestore } from "../firebase/config";
 
 const getCollection = (collection) => {
@@ -9,8 +9,10 @@ const getCollection = (collection) => {
     .collection(collection)
     .orderBy("createdAt");
 
-  collectionRef.onSnapshot(
+  const unsubscribe = collectionRef.onSnapshot(
     (snap) => {
+      console.log("snapshot");
+
       let results = [];
       snap.docs.forEach((doc) => {
         doc.data().createdAt && results.push({ ...doc.data(), id: doc.id });
@@ -24,6 +26,14 @@ const getCollection = (collection) => {
       error.value = "Could not fetch data";
     }
   );
+
+  watchEffect((onInvalidate) => {
+    // unsubscribe from previous collection when watcher is stopped (component unmounted)
+    onInvalidate(() => {
+      // console.log("watcher disabled");
+      unsubscribe();
+    });
+  });
 
   return { documents, error };
 };
